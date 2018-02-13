185c808e91d23e7c630937df123c8bf3d3ce568f
njang <neo.w.jang@gmail.com>
2018-02-13 15:14:31 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './images/fruits.svg';
+import heroImage from './images/caroline-attwood-225496.jpg'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App hero-image">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Neighborful</h1>
+        </header>
+        <img src={heroImage} className='img-responsive' width='100%'/>
+      </div>
+    );
+  }
+}
+
+export default App;
