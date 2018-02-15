9fa6602cb416d1e27ad048c9d2e94573848d5aea
njang <neo.w.jang@gmail.com>
2018-02-15 14:15:41 -0600

    Initialize main_app and views

--- Neighborful/urls.py
+++ Neighborful/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include, url
+from main_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	url(r'admin/', admin.site.urls),
+	# add the line below to your urlpatterns array
+	url(r'', views.index)
 ]
