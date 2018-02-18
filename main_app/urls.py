7681bb36428c9326cea2454ca4de1529e20b8d66
njang <neo.w.jang@gmail.com>
2018-02-18 01:01:43 -0600

    Refactor urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
-from .views import index, about, show, profile, sell_form, post_produce, dashboard, marketplace, login_view, logout_view
+from .views import index, about, show, profile, sell_form, post_produce, marketplace, login_view, logout_view
 
 urlpatterns = [
     url(r'^$', index),					# to main landing page
     url(r'^about/$', about),			# to about page
     url(r'^user/(\w+)/$', profile, name='profile'),				# display user profile page
     url(r'^marketplace/$', marketplace, name='marketplace'),	# display marketplace showing all produces available for sale
-    url(r'^([0-9]+)/$', show, name="show"),						# display a single produce by id
+    url(r'^([0-9]+)/$', show, name='show'),						# display a single produce by id
     url(r'^sell/$', sell_form),									# display a sales template for a produce
     url(r'^sell/post_url/$', post_produce, name='post_produce'),# route to post a new produce up for sale
-	url(r'^login/$', login_view, name="login"),		# display login page
-	url(r'^logout/$', logout_view, name="logout"),	# route to logout
+	url(r'^login/$', login_view, name='login'),		# display login page
+	url(r'^logout/$', logout_view, name='logout'),	# route to logout
 ]
