36d6b150dc6175549062dbc2749134b5b1711a9e
AleenaMad <aleena.madni@gmail.com>
2018-02-20 17:57:58 -0600

    added functional search, search template, updated models, views, url

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+#Manage the model
+class ProduceManager(models.Manager):
+    def active(self, *args, **kwargs):
+        return super(ProduceManager, self)
+
 class Produce(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -9,6 +15,11 @@ class Produce(models.Model):
     # buyer = models.ForeignKey(User, on_delete=models.PROTECT, related_name='buyer')
     buyer = models.CharField(max_length=100)
 
+    objects = ProduceManager()
+
 
     def __str__(self):
     	return self.name
+
+class Meta:
+    ordering = ["-id", "-timestamp", "-updated"]
