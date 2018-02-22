a1547655e53a787f0c536981c44a695cfb547662
njang <neo.w.jang@gmail.com>
2018-02-22 01:15:42 -0600

    Display balance in admin console

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Produce, Address
+from .models import Produce, Address, Balance
 
 # Register your models here.
 admin.site.register(Produce)
 admin.site.register(Address)
+admin.site.register(Balance)
